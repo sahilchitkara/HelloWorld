6d93ea6c93bfcc30abb54d9efb7bcfa65fc8beb2
Sahil Chitkara <sahil.chitkara91@gmail.com>
2014-08-30 11:34:55 +0530

    removed :p from text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'This is Hello world Application :P' ,value:"Hello World"});
+  res.render('index', { title: 'This is Hello world Application' ,value:"Hello World"});
 });
 
 module.exports = router;
